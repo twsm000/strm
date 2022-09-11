2bc004a6977479dfba3e7baacb0e7b86dae0db52
Thomas M <twsm000@gmail.com>
2022-09-11 12:58:45 -0300

    Add ConfineEnqueue and test

--- confine.go
+++ confine.go
@@ -12,3 +12,16 @@ func Confine[T any](process StreamProcessor[T]) StreamReceiver[T] {
 
 	return receiver
 }
+
+// ConfineEnqueue confine a queue stream process and automatically close the receiver
+// after the job is done
+func ConfineEnqueue[T any](queueSize int, process StreamProcessor[T]) StreamReceiver[T] {
+	sender, receiver, free := NewStreamBuffer[T](queueSize)
+
+	go func() {
+		defer free()
+		process(sender)
+	}()
+
+	return receiver
+}
--- confine_test.go
+++ confine_test.go
@@ -21,3 +21,26 @@ func TestConfine(t *testing.T) {
 
 	assert.ObjectsAreEqualValues(expected, got)
 }
+
+func TestConfineEnqueue(t *testing.T) {
+	expected := []int{1, 2, 3, 4, 5}
+	stream := ConfineEnqueue(5, func(sender StreamSender[int]) {
+		for loopCount := 1; loopCount <= 2; loopCount++ {
+			for _, v := range expected {
+				output := v * loopCount
+				if output == 6 {
+					break
+				}
+
+				sender <- output
+			}
+		}
+	})
+
+	var got []int
+	for value := range stream {
+		got = append(got, value)
+	}
+
+	assert.ObjectsAreEqualValues(append(expected, 2, 4), got)
+}
